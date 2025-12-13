1008c60a8929898b1f5a71435bb54f4894b32aec
NoName <19268069+gittycondo@users.noreply.github.com>
2025-12-13 08:38:34 -0600

    Added appearance logic to ExitPortal.cs and added ExitScene to build.

--- Assets/Scripts/ExitPortal.cs
+++ Assets/Scripts/ExitPortal.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class ExitPortal : MonoBehaviour
 {
     public GameObject exitPortal;
+    public PlayerControll player;
+    public WheatMaze maze;
 
     // Start is called before the first frame update
     void Awake()
@@ -17,7 +19,16 @@ public class ExitPortal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.crypto_count >= 20)
+        {
+            print("Portal may now appear.");
+            //Instantiate(exitPortal, maze.exitTile.transform.position, maze.exitTile.transform.rotation);
+        }
+        else
+        {
+            print("Portal won't appear yet. ");
+            print(player.crypto_count);
+        }
     }
 
     void OnTriggerEnter(Collider other)
